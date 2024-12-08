ad183b0c63e8623fa7162a6cf0e6da8734df8b46
benagastov <agastamarunda@gmail.com>
2024-12-08 05:13:55 +0000

    Add project files and build script for Cloudflare Pages

--- BlazorOnGitHubPages/obj/Release/netstandard2.1/RazorDeclaration/Pages/Index.razor.g.cs
+++ BlazorOnGitHubPages/obj/Release/netstandard2.1/RazorDeclaration/Pages/Index.razor.g.cs
@@ -87,6 +87,69 @@ using BlazorOnGitHubPages.Shared
         {
         }
         #pragma warning restore 1998
+#nullable restore
+#line 12 "/content/BlazorOnGitHubPages/Pages/Index.razor"
+       
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await JSRuntime.InvokeVoidAsync("eval", @"
+                (function() {
+                    const ctx = document.getElementById('stockChart');
+                    
+                    const data = {
+                        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+                        datasets: [{
+                            label: 'Stock Price (USD)',
+                            data: [150, 165, 155, 180, 175, 190],
+                            borderColor: 'rgb(75, 192, 192)',
+                            tension: 0.1
+                        }]
+                    };
+
+                    const config = {
+                        type: 'line',
+                        data: data,
+                        options: {
+                            responsive: true,
+                            plugins: {
+                                title: {
+                                    display: true,
+                                    text: 'Monthly Stock Prices'
+                                }
+                            }
+                        }
+                    };
+
+                    new Chart(ctx, config);
+                })()
+            ");
+        }
+    }
+
+#line default
+#line hidden
+#nullable disable
+
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private 
+#nullable restore
+#line 2 "/content/BlazorOnGitHubPages/Pages/Index.razor"
+        IJSRuntime
+
+#line default
+#line hidden
+#nullable disable
+         
+#nullable restore
+#line 2 "/content/BlazorOnGitHubPages/Pages/Index.razor"
+                   JSRuntime
+
+#line default
+#line hidden
+#nullable disable
+         { get; set; }
+         = default!;
     }
 }
 #pragma warning restore 1591
